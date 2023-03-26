5b083243e202c49e34df013342841e2f1c3c51ae
felipendelicia <delicia4581@gmail.com>
2023-03-26 09:45:07 -0300

    handDraw

--- src/data/tools.tsx
+++ src/data/tools.tsx
@@ -1,19 +1,26 @@
-import { faLinesLeaning, faSquare } from "@fortawesome/free-solid-svg-icons"
+import { faLinesLeaning, faPencil, faSquare } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { createLine } from "../services/createLine"
-import { createRectangle } from "../services/createRectangle"
+import handDraw from "../services/createHandDraw"
+import line from "../services/createLine"
+import rectangle from "../services/createRectangle"
 
 const tools = [
     {
+        name: 'handdraw',
+        icon: <FontAwesomeIcon icon={faPencil}/>,
+        func: handDraw
+    },
+    {
         name: 'line',
         icon: <FontAwesomeIcon icon={faLinesLeaning}/>,
-        func: createLine
+        func: line
     },
     {
         name: 'rectangle',
         icon: <FontAwesomeIcon icon={faSquare}/>,
-        func: createRectangle
-    }
+        func: rectangle
+    },
+    
 ]
 
 export default tools
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,9 @@
 import { Dispatch, ReactElement, SetStateAction } from "react";
-import { Drawable } from "roughjs/bin/core";
 
-export interface IElement {
-    roughElement: Drawable;
-    x1: number;
-    y1: number;
-    x2: number;
-    y2: number;
-}
 export interface ICurrentTool {
     name: string,
     icon: ReactElement,
-    func: (x1:number, y1:number, x2:number, y2:number) => IElement,
+    func: { mainFunc: any, mouseDown: any, mouseMove: any, mouseUp: any },
 }
 
 
@@ -21,6 +13,6 @@ export interface StateCtx {
 }
 
 export interface Ctx {
-    elements: { roughElement: Drawable, x1: number, y1: number, x2: number, y2: number }[],
+    elements: any[],
     currentTool: ICurrentTool
 }
