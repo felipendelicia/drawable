18cdeabde513cb074d3afc237a06048943b305ba
felipendelicia <delicia4581@gmail.com>
2023-03-27 16:09:37 -0300

    update selection linear path algoritm

--- src/services/getElementAtPosition.ts
+++ src/services/getElementAtPosition.ts
@@ -1,3 +1,5 @@
+import { ADMITED_OFFSET } from "../constants";
+
 const distance = (a: { x: number, y: number }, b: { x: number, y: number }) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
 
 export function isWithinElement(x: number, y: number, element: any) {
@@ -20,9 +22,8 @@ export function isWithinElement(x: number, y: number, element: any) {
     } else if (shape === 'ellipse') {
         const { centerX, centerY, width, height } = element
         const offset = Math.pow(x - centerX, 2) / Math.pow(width / 2, 2) + Math.pow(y - centerY, 2) / Math.pow(height / 2, 2)
-        return offset <= 1
+        return offset <= ADMITED_OFFSET
     } else if (shape === 'linearPath') {
-        const ADMITED_OFFSET = 3
         const { path } = element
         for (let i = 0; i < path.length - 1; i++) {
             let x1 = path[i][0];
@@ -30,9 +31,11 @@ export function isWithinElement(x: number, y: number, element: any) {
             let x2 = path[i + 1][0];
             let y2 = path[i + 1][1];
 
-            let distancia = Math.abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1) / Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
+            // let distancia = Math.abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1) / Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
 
-            if (distancia <= ADMITED_OFFSET) return true
+            let distancia = distance({ x: x1, y: y1 }, { x: x2, y: y2 }) - (distance({ x: x1, y: y1 }, { x, y }) + distance({ x: x2, y: y2 }, { x, y }))
+
+            if (Math.abs(distancia) <= ADMITED_OFFSET) return true
 
         }
         return false
